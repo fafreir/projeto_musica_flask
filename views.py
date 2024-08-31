d331b7424dd3da6d61c8edc96c2743d860328a13
Fabrício Adriel <fabricioadriel@yahoo.com.br>
2024-08-31 20:55:18 -0300

    Criado views e atualizado

---
+++ views.py
@@ -0,0 +1,69 @@
+from flask import Flask, render_template, request, redirect, session, flash, url_for
+from models import Musica, Usuario
+from musica import app, db
+
+
+@app.route('/')
+def listarMusicas():
+    if session['usuario_logado'] == None or 'usuario_logado' not in session:
+        return redirect(url_for('login'))
+    lista = Musica.query.order_by(Musica.id_musica)
+    return render_template('lista_musicas.html', titulo='Musicas cadastradas', musicas=lista)
+
+
+@app.route('/cadastrar')
+def cadastrar_musica():
+    if session['usuario_logado'] == None or 'usuario_logado' not in session:
+        return redirect(url_for('login'))
+    return render_template('cadastra_musica.html', titulo="Cadastrar música")
+
+
+@app.route('/adicionar', methods=['POST',])
+def adicionar_musica():
+    nome = request.form['txtNome']
+    cantorBanda = request.form['txtCantor']
+    genero = request.form['txtGenero']
+
+    musica = Musica.query.filter_by(nome_musica=nome).first()
+
+    if musica:
+        flash("Musica já está cadastrada!")
+        return redirect(url_for('listarMusicas'))
+
+    nova_musica = Musica(
+        nome_musica=nome, cantor_banda=cantorBanda, genero_musica=genero)
+    db.session.add(nova_musica)
+    db.session.commit()
+    return redirect(url_for('listarMusicas'))
+
+
+@app.route('/login')
+def login():
+    return render_template('login.html')
+
+
+@app.route('/autenticar', methods=['POST',])
+def autenticar():
+
+    usuario = Usuario.query.filter_by(
+        login_usuario=request.form['txtLogin']).first()
+    if usuario:
+        if request.form['txtSenha'] == usuario.senha_usuario:
+            session['usuario_logado'] = request.form['txtLogin']
+            flash(
+                f"Seja bem-vindo {usuario.login_usuario} logado com sucesso!")
+            return redirect(url_for('listarMusicas'))
+        else:
+            flash("Senha inválida!")
+            return redirect(url_for('login'))
+    else:
+        flash("Usuário ou senha inválida!")
+        # return redirect('/login')
+        return redirect(url_for('login'))
+
+
+@app.route('/sair')
+def sair():
+    session['usuario_logado'] = None
+
+    return redirect(url_for('login'))
