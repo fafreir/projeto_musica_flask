c77c257fdb502fbb289c53637495cfb8f5bfc927
Fabrício Adriel <fabricioadriel@yahoo.com.br>
2024-09-01 08:37:42 -0300

    adicionado caminho dos arquivos

--- config.py
+++ config.py
@@ -1,3 +1,5 @@
+import os
+
 SECRET_KEY = 'aprendendodoiniciocomdaniel'
 
 SQLALCHEMY_DATABASE_URI = \
@@ -8,3 +10,5 @@ SQLALCHEMY_DATABASE_URI = \
         servidor='localhost',
         database='playmusica'
     )
+
+UPLOAD_PASTA = os.path.dirname(os.path.abspath(__file__)) + '/uploads'
