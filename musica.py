e5abe45beb1b34bf8138d288e036b14e7e7b4b90
Fabrício Adriel <fabricioadriel@yahoo.com.br>
2024-09-04 22:29:45 -0300

    Token

--- musica.py
+++ musica.py
@@ -1,5 +1,6 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from flask_wtf.csrf import CSRFProtect
 
 app = Flask(__name__)
 
@@ -7,6 +8,7 @@ app.config.from_pyfile('config.py')
 
 db = SQLAlchemy(app)
 
+csrf = CSRFProtect(app)
 from views import *
 
 if __name__ == '__main__':
