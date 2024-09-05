6db947c209f6877e32e03fde4dbb8915acda05d3
Fabrício Adriel Coelho Freire <fabricioadriel@yahoo.com.br>
2024-09-05 15:33:52 -0300

    Atualizado

--- musica.py
+++ musica.py
@@ -1,15 +1,17 @@
-from views_musica import *
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_wtf.csrf import CSRFProtect
+
 
 app = Flask(__name__)
 
 app.config.from_pyfile('config.py')
 
 db = SQLAlchemy(app)
-
 csrf = CSRFProtect(app)
+
+from views_musica import *
+from views_usuario import *
 
 if __name__ == '__main__':
     app.run(debug=True)
